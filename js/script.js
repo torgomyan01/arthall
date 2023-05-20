bf8140bc157d21e7cec6f3c136cd246dcc2029ef
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-05-21 00:23:04 +0400

    End project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-const { none } = {
-    none: "d-none"
+const { none, show } = {
+    none: "d-none",
+    show: 'show'
 }
 
 
@@ -11,4 +12,17 @@ $('.change-name-user-profile').on('click', function (){
 $('.save-user-info').on('click', function (){
     $('.user-profile-name').removeClass(none);
     $('.form-change-name-user').addClass(none)
+})
+
+$('.close-green-message').on('click', function (){
+    $(this).parent().addClass(none)
+})
+
+
+$('.open-green-message').on('click', function (){
+    $('.green-message').addClass(show);
+
+    setTimeout(() => {
+        $('.green-message').removeClass(show);
+    }, 3000)
 })
