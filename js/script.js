8a599a1123ade095a4044e237d46a2ce606aa5d8
torgomyan1998 <andranik.torgomyan1998@gmail.com>
2023-05-22 11:10:10 +0400

    Add sign up functionaliti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const {none, show, active} = {
     show: 'show',
     active: 'active'
 }
-const Button = $('.btn-sign')
 
 $('.change-name-user-profile').on('click', function () {
     $('.user-profile-name').addClass(none);
@@ -28,9 +27,14 @@ $('.open-green-message').on('click', function () {
     }, 3000)
 })
 
+const Button = $('.btn-sign')
+const tabSignUp = $('.sign-up-tab');
+
 Button.on('click', function () {
     Button.removeClass(active);
     $(this).addClass(active);
+    tabSignUp.addClass(none);
+    $(`.sign-up-tab[data-tabname="${$(this).text()}"]`).removeClass(none)
 })
 
 
@@ -39,22 +43,31 @@ $('.select-image-crete-work').on('change', function (e) {
     $(this).prev().prev('span').text(files[0] ? files[0]?.name : 'Выберите изображение в формате PNG или JPG')
 })
 
+
+$('.sign-up-image').on('change', function (e) {
+    const files = e.target.files;
+    $('.sign-up-image-text').html(files[0] ? files[0]?.name : 'Файл в формате <br> PNG или JPG')
+})
+
+
 const dataMask = document.getElementById('data-mask');
 const dataMaskEnd = document.getElementById('data-mask-end');
 
-IMask(dataMask,
-    {
-        mask: Date,
-        min: new Date(1990, 0, 1),
-        max: new Date(),
-        lazy: false
-    });
+if(dataMask && dataMaskEnd){
+    IMask(dataMask,
+        {
+            mask: Date,
+            min: new Date(1990, 0, 1),
+            max: new Date(),
+            lazy: false
+        });
 
-IMask(dataMaskEnd,
-    {
-        mask: Date,
-        min: new Date(1990, 0, 1),
-        max: new Date(2050, 0, 1),
-        lazy: false
-    });
+    IMask(dataMaskEnd,
+        {
+            mask: Date,
+            min: new Date(1990, 0, 1),
+            max: new Date(2050, 0, 1),
+            lazy: false
+        });
+}
 
